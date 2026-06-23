a2cf798576a3a151dd1fbd0f82794670f70cc00c
Arjan <Arjan.van.Vught@gmail.com>
2026-06-23 14:06:21 +0200

    Update header comments, add uart.h and usb host license
    Normalize file header tags across board sources (replace hal_* @file tags with board_*), correct font_cp437.h @file tag, and minor API change in ILI9341::SetBackLight to use gpio::Write. Add a new uart.h GD32 wrapper providing inline UART helpers and add licensing/header and API cleanups to lib-gd32/include/device/usb/host.h (namespace formatting, enums, function prototypes). Suppress -Wunused-parameter for GD32H7 includes and add EXTRA_INCLUDES to lib-hwclock/Rules.mk to include board headers.

--- lib-gd32/include/device/usb/host.h
+++ lib-gd32/include/device/usb/host.h
@@ -1,43 +1,51 @@
-/*
- * host.h
+/**
+ * @file host.h
+ */
+/* Copyright (C) 2026 by Arjan van Vught mailto:info@gd32-dmx.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 #ifndef DEVICE_USB_HOSTMSC_H_
 #define DEVICE_USB_HOSTMSC_H_
 
-
 namespace usb::host {
-
-enum class Status {
-	NOT_AVAILABLE,
-	DISCONNECTED,
-	RESET,
-	SPEED_AVAILALBE,
-	ATTACHED,
-	CONNECTED,
-	ENUMERATION_COMPLETED,
-	READY,
-	DEVICE_NOT_SUPPORTED,
-	UNRECOVERABLE_ERROR
+enum class Status { 
+  NOT_AVAILABLE, 
+  DISCONNECTED, 
+  RESET, 
+  SPEED_AVAILALBE, 
+  ATTACHED, 
+  CONNECTED, 
+  ENUMERATION_COMPLETED, 
+  READY, 
+  DEVICE_NOT_SUPPORTED, 
+  UNRECOVERABLE_ERROR 
 };
 
-enum class Speed {
-	FAULT,
-	LOW,
-	FULL,
-	HIGH
-};
+enum class Speed { FAULT, LOW, FULL, HIGH };
 
-enum class Class {
-	NOT_SUPPORTED,
-	MSC,
-	HID
-};
+enum class Class { NOT_SUPPORTED, MSC, HID };
 
 Status get_status();
 Speed get_speed();
 Class get_class();
 } // namespace usb::host
 
-
-#endif /* DEVICE_USB_HOSTMSC_H_ */
+#endif // DEVICE_USB_HOSTMSC_H_
